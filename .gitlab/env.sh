d62cd211fc243b8a5c3c87a411035a17c3c82647
kwiss <christophe@hooraylabs.com>
2020-02-29 15:11:29 +0100

    edit conf

---
+++ .gitlab/env.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#
+export BRANCH_NAME=${BRANCH_NAME:=$CI_COMMIT_REF_SLUG}
+export HASH_SIZE=${HASH_SIZE:=7}
+
+BRANCH_NAME_HASHED=$( printf "${BRANCH_NAME}" | sha1sum | cut -c1-${HASH_SIZE} )
+export BRANCH_HASH=${BRANCH_HASH:="$BRANCH_NAME_HASHED"}
+export K8S_NAMESPACE="gob-${BRANCH_HASH}"
