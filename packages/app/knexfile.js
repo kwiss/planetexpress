fe59dffc8d0402e5ccb48c9c811ff1e11562ac38
kwiss <christophe@hooraylabs.com>
2020-03-10 19:52:08 +0100

    fix(ssl): log db

--- packages/app/knexfile.js
+++ packages/app/knexfile.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const databaseName = "planetexpress_dev";
 
+console.log(process.env.DATABASE_URL)
+
 const connectionUrl =
   process.env.DATABASE_URL ||
   `postgres://postgres:test@localhost:5434/${databaseName}`;
